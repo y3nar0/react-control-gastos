2c8270ae996acb0227e2b311f9f8057fd14faa7c
Genaro OG <genaro.osorio@icoddes.com>
2022-06-11 18:45:56 -0500

    mostrando gasto en componente ListadoGastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Header from "./components/Header"
+import ListadoGastos from "./components/ListadoGastos"
 import Modal from "./components/Modal"
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -24,6 +25,7 @@ function App() {
 
   const guardarGato = gasto => {
     gasto.id = generarId()
+    gasto.fecha = Date.now()
     setGastos([...gastos, gasto])
 
     setAnimarModal(false)
@@ -43,13 +45,20 @@ function App() {
 
       {
         isValidPresupuesto && (
-          <div className="nuevo-gasto">
-            <img 
-              src={IconoNuevoGasto} 
-              alt="icono nuevo gasto" 
-              onClick={handleNuevoGasto}
-            />
-          </div>
+          <>
+            <main>
+              <ListadoGastos 
+                gastos={gastos}
+              />
+            </main>
+            <div className="nuevo-gasto">
+              <img 
+                src={IconoNuevoGasto} 
+                alt="icono nuevo gasto" 
+                onClick={handleNuevoGasto}
+              />
+            </div>
+          </>
         )
       }
 
