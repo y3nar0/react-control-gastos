15d52717860930508b002e6218f4064348247647
Genaro OG <genaro.osorio@icoddes.com>
2022-06-13 22:37:53 -0500

    Mostrando Filtros por Categoría en la Interfaz

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,18 +1,44 @@
 import Gasto from "./Gasto"
 
-const ListadoGastos = ({ setGastoEditar, gastos, eliminarGasto }) => {
+const ListadoGastos = ({ 
+  setGastoEditar, 
+  gastos, 
+  eliminarGasto,
+  filtro,
+  gastosFiltrados
+ }) => {
   return (
     <div className='listado-gastos contenedor'>
-        <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
 
-        {gastos.map( gasto => (
-            <Gasto 
-                key={gasto.id}
-                gasto={gasto}
-                setGastoEditar={setGastoEditar}
-                eliminarGasto={eliminarGasto}
-            />
-        ))}
+        {
+          filtro ? (
+            <>
+              <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos en esta categoría'}</h2>
+
+              {gastosFiltrados.map( gasto => (
+                <Gasto 
+                    key={gasto.id}
+                    gasto={gasto}
+                    setGastoEditar={setGastoEditar}
+                    eliminarGasto={eliminarGasto}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+
+              {gastos.map( gasto => (
+                <Gasto 
+                    key={gasto.id}
+                    gasto={gasto}
+                    setGastoEditar={setGastoEditar}
+                    eliminarGasto={eliminarGasto}
+                />
+              ))}
+            </>
+          )
+        }
     </div>
   )
 }
